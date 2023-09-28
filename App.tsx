72238b9b928821dfbec84888ec3e656d1870f415
Francisco Jarma <francisco.jarma@deltatre.com>
2023-09-28 10:38:37 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { Text, View, StyleSheet, Touchable, TouchableOpacity, Alert } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { styles } from './Global-styles';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+    'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+    'Lato-Thin': require('./assets/fonts/Lato-Thin.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity onPress={() => Alert.alert('Button touched!')}>
+        <Text style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, minima voluptatum doloremque numquam omnis fugiat, est porro quae totam sed aut libero rerum incidunt accusantium? Quibusdam officiis numquam sequi harum?</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Global-styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF8F0',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontFamily: 'Lato-Regular'
+    }
+});
